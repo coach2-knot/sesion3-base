95acde2144ea672e91190e6828ff46c5e5193c5d
Arnol Ramirez Arevalo <aramirez@octa-data.com>
2026-02-24 19:12:43 -0500

    add inventory

--- src/test/java/com/knotacademy/qc/selenium/pages/InventoryPage.java
+++ src/test/java/com/knotacademy/qc/selenium/pages/InventoryPage.java
@@ -1,20 +1,48 @@
 package com.knotacademy.qc.selenium.pages;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;;
 
 public class InventoryPage extends BasePage {
 
     private final By pageTitle = By.className("span.title");
     /// span[contains(text(),"Products")]
+    private final By addBackpackButton = By.id("add-to-cart-sauce-labs-backpack");
+    private final By addTShirt = By.id("add-to-cart-sauce-labs-bolt-t-shirt");
+    private final By cartBadge = By.cssSelector(".shopping_cart_link .shopping_cart_badge");
 
     public InventoryPage(WebDriver driver, WebDriverWait wait) {
         super(driver, wait);
     }
 
     public boolean isLoaded() {
-        ///
         return "Products".equals(text(pageTitle));
     }
+
+    public void addBackpack(){
+        click(addBackpackButton);
+    }
+
+    public void addTShirt(){
+        click(addTShirt);
+    }
+
+    public int getCartBadgeCount(){
+        List<WebElement> badges = driver.findElements(cartBadge);
+        if (badges.isEmpty()){
+            return 0;
+        }
+
+        String value = badges.get(0).getText();
+        if (value == null || value.isBlank()){
+            return 0;
+        }
+
+        return Integer.parseInt(value.trim());
+    }
+
 }
--- src/test/java/com/knotacademy/qc/selenium/tests/SauceDemoPomTest.java
+++ src/test/java/com/knotacademy/qc/selenium/tests/SauceDemoPomTest.java
@@ -1,10 +1,12 @@
 package com.knotacademy.qc.selenium.tests;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
 import com.knotacademy.qc.selenium.core.BaseTest;
+import com.knotacademy.qc.selenium.pages.InventoryPage;
 import com.knotacademy.qc.selenium.pages.LoginPage;
 
 public class SauceDemoPomTest extends BaseTest {
@@ -12,11 +14,18 @@ public class SauceDemoPomTest extends BaseTest {
     @Test
     void login(){
         LoginPage loginPage = new LoginPage(driver, wait);
+        InventoryPage inventoryPage = new InventoryPage(driver, wait);
 
         loginPage.open(requiredProperty("base.url"));
         loginPage.login(requiredProperty("username"),requiredProperty("password"));
 
-        //assertTrue(null);
+        assertTrue(inventoryPage.isLoaded(),"Despues de login siempre validamos productos / inventario");
+
+        inventoryPage.addBackpack();
+        inventoryPage.addTShirt();
+        assertEquals(2, inventoryPage.getCartBadgeCount(),"El badge debe tener dos productos");
+
+
     }
     
 }
